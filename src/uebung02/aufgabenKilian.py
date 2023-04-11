7309f52f9ae76e6db095897ca43ae63725d57c3d
Kilian Horn <kilian.horn@inform-software.com>
2023-04-11 14:08:08 +0200

    Funktioniert, aber keine Garantie auf mathematische Korrektheit.

--- src/uebung02/aufgabenKilian.py
+++ src/uebung02/aufgabenKilian.py
@@ -1,10 +1,8 @@
-# entweder verwende ich numpy ganz oder gar nicht. Danke.
 
 def zerlegung(matrix: list[list]):
     perms = []
     for i in range(matrix.__len__()):
-        print(i, matrix)
-        res = permutation(matrix, i)
+        res = rowswap(matrix, i)
         if res == "invalid matrix":
             return "invalid matrix"
         matrix = res[0]
@@ -18,7 +16,7 @@ def zerlegung(matrix: list[list]):
     return matrix, perms
 
 
-def permutation(matrix: list[list], index: int):
+def rowswap(matrix: list[list], index: int):
     const = index
 
     while matrix[index][const] == 0:
@@ -31,5 +29,61 @@ def permutation(matrix: list[list], index: int):
     return [matrix, index]
 
 
+def permutation(p: list, b: list):
+    for i in range(p.__len__()):
+        # wunderhübscher tausch von elementen des vektors
+        temp = b[i]
+        b[i] = b[p[i]]
+        b[p[i]] = temp
+    return b
+
+
+def vorwaerts(LU: list[list], c: list):
+    ret = []
+    for i in range(LU.__len__()):
+        ret.append(c[i])
+        for j in range(i):
+            ret[i] -= LU[i][j]*ret[j]
+    return ret
+
+
+def rueckwaerts(LU: list, y: list):
+    ret = []
+    for i in reversed(range(LU.__len__())):
+        ret.append(y[i])
+        for j in reversed(range(LU.__len__(), i)):
+            ret[ret.__len__()-1] -= LU[i][j]*ret[j]
+    return ret
+
+
+def matrixgen(size: int):
+    ret = []
+    for i in range(size):
+        ret.append([])
+        for j in range(size):
+            ret[i].append(1/(i+j+1))
+    return ret
+
+
+def bgen(size: int):
+    ret = []
+    for i in range(size):
+        ret.append(1/(i+1))
+    return ret
+
+
 matrix = [[0, 0, 0, 1], [2, 1, 2, 0], [4, 4, 0, 0], [2, 3, 1, 0]]
-print(zerlegung(matrix))
+zerl = zerlegung(matrix)
+print(zerl)
+vorw = vorwaerts(zerl[0], permutation(zerl[1], [3, 5, 4, 5]))
+print(vorw)
+rueck = rueckwaerts(zerl[0], vorw)
+print(rueck)
+matrix = matrixgen(10)
+zerl = zerlegung(matrix)
+print(zerl)
+b = bgen(10)
+vorw = vorwaerts(zerl[0], permutation(zerl[1], b))
+print(vorw)
+rueck = rueckwaerts(zerl[0], vorw)
+print(rueck)
